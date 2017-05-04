8546f0fed00c5d2d2bd93666574a8cf2a21ffbb6
Chris <shadowace112@live.com>
2017-05-04 15:03:03 -0600

    rotation attempt 3

--- src/utils/Tile.ts
+++ src/utils/Tile.ts
@@ -7,7 +7,13 @@ export class Tile extends Point{
         return 'land_3way';
     }
     get rotation(){
-        return 90;
+        return 270;
+    }
+    get printX(){
+        return this.x*Math.sin((this.rotation/180)*Math.PI);
+    }
+    get printY(){
+        return this.y*Math.cos((this.rotation/180)*Math.PI);
     }
     get entrances(){
         return this.__entrances
