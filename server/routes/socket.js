a95b5ce07bb9c6891e4e38ecc55fa4575da6a4f7
Chris <Shadowace248@gmail.com>
2017-05-10 09:37:20 -0600

    genTileAttempt2

--- server/routes/socket.js
+++ server/routes/socket.js
@@ -26,7 +26,7 @@ module.exports = function(io) {
 };
 
 function genGameBoard(callback){
-    let nodes = genNodes(function(nodes){
+    genNode(16, 16, [], function(nodes){
         let tiles = [];
         nodes.map(function(node, i){
             tiles.push({
@@ -39,70 +39,84 @@ function genGameBoard(callback){
         callback(tiles);
     });
 }
-function genNodes(callback){
-    nodes = [];
-    nodes.push({x:8, y:8, sides:[1,1,1,1]});
-    let isEmptySides = false;
-    while(!isEmptySides){
-        isEmptySides = true;
-        nodes.map(function(node, i){
-            if(node.sides[1] && !checkForNode(node.x, node.y-1, nodes)){
-                nodes.push(genNode(node.x, node.y-1));
-                isEmptySides = false;
-            }
-            if(node.sides[2] && !checkForNode(node.x+1, node.y, nodes)){
-                nodes.push(genNode(node.x+1, node.y));
-                isEmptySides = false;
-            }
-            if(node.sides[3] && !checkForNode(node.x, node.y+1, nodes)){
-                nodes.push(genNode(node.x, node.y+1));
-                isEmptySides = false;
-            }
-            if(node.sides[4] && !checkForNode(node.x-1, node.y, nodes)){
-                nodes.push(genNode(node.x-1, node.y));
-                isEmptySides = false;
-            }
-        });
-        console.log('currennt number of nodes: ' + nodes.length);
-    }
-    callback(nodes);
-}
-function genNode(x, y){
+// function genNodes(callback){
+//     nodes = [];
+//     nodes.push({x:8, y:8, sides:[1,1,1,1]});
+//     let isEmptySides = false;
+//     while(!isEmptySides){
+//         isEmptySides = true;
+//         nodes.map(function(node, i){
+//             if(node.sides[1] && !checkForNode(node.x, node.y-1, nodes)){
+//                 nodes.push(genNode(node.x, node.y-1));
+//                 isEmptySides = false;
+//             }
+//             if(node.sides[2] && !checkForNode(node.x+1, node.y, nodes)){
+//                 nodes.push(genNode(node.x+1, node.y));
+//                 isEmptySides = false;
+//             }
+//             if(node.sides[3] && !checkForNode(node.x, node.y+1, nodes)){
+//                 nodes.push(genNode(node.x, node.y+1));
+//                 isEmptySides = false;
+//             }
+//             if(node.sides[4] && !checkForNode(node.x-1, node.y, nodes)){
+//                 nodes.push(genNode(node.x-1, node.y));
+//                 isEmptySides = false;
+//             }
+//         });
+//         console.log('currennt number of nodes: ' + nodes.length);
+//     }
+//     callback(nodes);
+// }
+function genNode(x, y, otherNodes, callback){
     console.log('making node at '+x +':'+y)
     let thisNode = {x:x, y:y, sides:[]};
-    for(let node of nodes){
-        if(node.x==(x-1)&&node.y==(y)){
+    for(let otherNode of otherNodes){
+        if(otherNode.x==(x-1)&&otherNode.y==(y)){
             thisNode.sides[4] = 1;
+        }else{
+            thisNode = setSideValue(thisNode, 4);
         }
-        if(node.x==(x+1)&&node.y==(y)){
+        if(otherNode.x==(x+1)&&otherNode.y==(y)){
             thisNode.sides[2] = 1;
+        }else{
+            thisNode = setSideValue(thisNode, 4);
         }
-        if(node.x==(x)&&node.y==(y-1)){
+        if(otherNode.x==(x)&&otherNode.y==(y-1)){
             thisNode.sides[1] = 1;
+        }else{
+            thisNode = setSideValue(thisNode, 4);
         }
-        if(node.x==(x)&&node.y==(y+1)){
+        if(otherNode.x==(x)&&otherNode.y==(y+1)){
             thisNode.sides[3] = 1;
+        }else{
+            thisNode = setSideValue(thisNode, 4);
         }
     }
     thisNode.sides.map(function(isSideAnEntrance, i){
-        if(!isSideAnEntrance){
-            let rand=Math.ceil(Math.random()*100);
-            if(rand < 33){
-                thisNode.sides[i] = 1;
+        if(isSideAnEntrance){
+            if(i==0){
+                
             }
         } 
     });
     return thisNode;
 }
-function checkForNode(x, y, nodes){
-    let returnValue = false;
-    for(let node of nodes){
-        if(node.x==x&&node.y==y){
-            returnValue = node;
-        }
+function setSideValue(node, sideNumber){
+    let rand=Math.ceil(Math.random()*100);
+    if(rand < 33){
+        node.sides[sideNumber] = 1;
     }
-    return returnValue;
+    return node;
 }
+// function checkForNode(x, y, nodes){
+//     let returnValue = false;
+//     for(let node of nodes){
+//         if(node.x==x&&node.y==y){
+//             returnValue = node;
+//         }
+//     }
+//     return returnValue;
+// }
 
 function Wrapper(value, callback) {
     this.value = value;
