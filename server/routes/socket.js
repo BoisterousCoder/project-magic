cc9036d02ee3cafc8ee8b7f9fe8709f8fcb5ac6e
Chris <shadowace112@live.com>
2017-05-11 15:52:29 -0600

    iterative node generation

--- server/routes/socket.js
+++ server/routes/socket.js
@@ -19,7 +19,7 @@ module.exports = function(io) {
             let tileId = 0;
             genGameBoard(function(tileData){
                 if(tileData.entrances.toString() !== [0,0,0,0].toString()){
-                    console.log('sending data...');
+                    console.log('sending tiles to client...');
                     tileData.id = tileId;
                     socket.emit('setTile', JSON.stringify(tileData));
                     tileId++;
@@ -30,79 +30,113 @@ module.exports = function(io) {
 };
 
 function genGameBoard(callback){
-    genNode(8, 8, [], function(node){
+    let tiles = [];
+    let nodes = [genNode(8, 8, [])];
+    do{
+        let output = iterateNodes(nodes);
+        nodes = output[0];
+        if(output[1]){
+            break;
+        }
+    }while(true)
+    console.log('finnished genning tiles');
+    for(let node of nodes){
         tile = {
             x:node.x,
             y:node.y,
             entrances:node.sides,
             color:'green'
         };
+        
+        tiles.push(tile);
         callback(tile);
-    });
+    }
 }
-function genNode(x, y, otherNodes, callback){
+
+function genNode(x, y, otherNodes){
     console.log('making node at '+x +':'+y)
-    let thisNode = {x:x, y:y, sides:[]};
+    let thisNode = {x:x, y:y, sides:[1, 1, 1, 1], openEntrances:-1};
     for(let otherNode of otherNodes){
         if(otherNode.x==(x)&&otherNode.y==(y-1)){
-            thisNode.sides[0] = 1;
+            thisNode.sides[0] = 0;
         }
         if(otherNode.x==(x+1)&&otherNode.y==(y)){
-            thisNode.sides[1] = 1;
+            thisNode.sides[1] = 0;
         }
         if(otherNode.x==(x)&&otherNode.y==(y+1)){
-            thisNode.sides[2] = 1;
+            thisNode.sides[2] = 0;
         }
         if(otherNode.x==(x-1)&&otherNode.y==(y)){
-            thisNode.sides[3] = 1;
+            thisNode.sides[3] = 0;
         }
     }
     for(let i = 0; i<4; i++){
         if(!thisNode.sides[i]){
-            thisNode = setSideValue(thisNode, otherNodes, i, callback);
+            thisNode = setSideValue(thisNode, otherNodes, i);
         }
     }
+    console.log(thisNode.openEntrances);
     otherNodes.push(thisNode);
     console.log(thisNode.sides);
-    // thisNode.sides.map(function(isSideAnEntrance, i){
-    //     if(isSideAnEntrance){
-            
-    //     } 
-    // });
-    callback(thisNode);
+    return thisNode;
 }
-function setSideValue(node, otherNodes, sideNumber, callback){
+
+function iterateNodes(nodes){
+    let isThereNewNode = false;
+    nodes.map(function(node){
+        node.sides.map(function(side, i){
+            if(side){
+                switch(i){
+                    case(0):
+                        nodes.push(genNode(node.x, node.y-1, nodes));
+                        isThereNewNode=true;
+                        break;
+                    case(1):
+                        nodes.push(genNode(node.x+1, node.y, nodes));
+                        isThereNewNode=true;
+                        break;
+                    case(2):
+                        nodes.push(genNode(node.x, node.y+1, nodes));
+                        isThereNewNode=true;
+                        break;
+                    case(3):
+                        nodes.push(genNode(node.x-1, node.y, nodes));
+                        isThereNewNode=true;
+                        break;
+                }
+            }
+        });
+    });
+    return [nodes, isThereNewNode];
+}
+
+function setSideValue(thisNode, otherNodes, sideNumber){
     console.log('genning side value')
-    if(node.x < 16 && node.x > 0 && node.y < 16 && node.y > 0){
+    if(thisNode.x < 16 && thisNode.x > 0 && thisNode.y < 16 && thisNode.y > 0 && 
+        thisNode.sides[sideNumber] != 0 && !thisNode.sides[sideNumber])
+    {
         let rand=Math.ceil(Math.random()*100);
         if(rand < 33){
-            node.sides[sideNumber] = 1;
-            console.log('branching...')
-            switch(sideNumber){
-                case(0):
-                    genNode(node.x, node.y-1, otherNodes, callback);
-                    areAllEntrancesClosed=false;
-                    break;
-                case(1):
-                    genNode(node.x+1, node.y, otherNodes, callback);
-                    areAllEntrancesClosed=false;
-                    break;
-                case(2):
-                    genNode(node.x, node.y+1, otherNodes, callback);
-                    areAllEntrancesClosed=false;
-                    break;
-                case(3):
-                    genNode(node.x-1, node.y, otherNodes, callback);
-                    areAllEntrancesClosed=false;
-                    break;
-            }
+            thisNode.sides[sideNumber] = 1;
         }else{
-            node.sides[sideNumber] = 0;
+            thisNode.sides[sideNumber] = 0;
         }
     }else{
-        node.sides = [0,0,0,0];
+        console.log('a node is out of bounds');
+        thisNode.sides = [0,0,0,0];
     }
-    return node;
+    return thisNode;
+}
+
+function checkForNode(x, y, nodes){
+    let isNodeAtLocation = false;
+    for(let node of nodes){
+        if(node.x == x && node.y == y){
+            isNodeAtLocation = true;
+            break;
+        }
+    }
+    return isNodeAtLocation;
 }
 
 function Wrapper(value, callback) {
